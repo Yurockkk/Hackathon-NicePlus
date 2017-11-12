1cdc19d5c3b6566fb18ac93397f949b3f0ad9ac1
Yurockkk <r00631026@ntu.edu.tw>
2017-11-12 09:51:03 -0500

    Implement fetchSentimentScore

--- Nice+/ViewController.swift
+++ Nice+/ViewController.swift
@@ -12,28 +12,77 @@ import Alamofire
 
 class ViewController: UIViewController {
     
-    let parameters: Parameters = [
-        "document": [
-            "content": "stupid ass",
-            "type": "PLAIN_TEXT"
-        ],
-        "encodingType": "UTF8"
-    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Alamofire.request("https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyCWF1-fJvUSlYBfx9K9HbO9b8ggQcZydk4", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON(completionHandler: { response in
-            print(response)
+        let post = "DT SCREW YOU!!! DT LOVE YOU!! DT EAT MY SHIT!! DT is the best!"
+        
+        self.fetchSentimentScore(inputString: post,completion: {
+            results in
+            print("\(post) -> score: \(results)")
         })
 
     }
+    
+    func fetchSentimentScore(inputString: String, completion: @escaping (Double)-> Void) {
+        let text = inputString
+        let parameters: Parameters = [
+            "document": [
+                "content": text,
+                "type": "PLAIN_TEXT"
+            ],
+            "encodingType": "UTF8"
+        ]
+        Alamofire.request("https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyCWF1-fJvUSlYBfx9K9HbO9b8ggQcZydk4", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON(completionHandler: { response in
+//            print(response)
+            guard let datalevel1 = response.result.value as? [String: Any] else {
+                print("Error")
+                return
+            }
+//            print(datalevel1)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            guard let dataLevel2 = datalevel1["documentSentiment"] as? [String: Any] else{
+                print("error2")
+                return
+            }
+//            print(dataLevel2)
+            
+            guard let score = dataLevel2["score"] as? Double else{
+                return
+            }
+            
+            //printout each sentence
+//            dataLevel2.forEach{ sentence in
+//                print(sentence)
+//                guard let scoreTuple = sentence["sentiment"] as? [String: Any] else{
+//                    return
+//                }
+//                guard let score = scoreTuple["score"] as? Double else {
+//                    return
+//                }
+//
+//                print(score)
+//            }
+            
+//            guard let dataLevel3 = dataLevel2[0]["sentiment"] as? [String: Any] else{
+//                print("error3")
+//                return
+//            }
+//            guard let scores = dataLevel3["score"] as? Double else{
+//                print("score error")
+//                return
+//            }
+            
+//            print(scores)
+            
+            completion(score)
+            })
+        
+        
     }
-
+    
 
 }
 
