b5f4d043978346e4df7905c3b3934af3da69dc4d
Yurockkk <r00631026@ntu.edu.tw>
2017-11-11 19:44:38 -0500

    INIT

--- Nice+/ViewController.swift
+++ Nice+/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
+    
+    let parameters: Parameters = [
+        "document": [
+            "content": "stupid ass",
+            "type": "PLAIN_TEXT"
+        ],
+        "encodingType": "UTF8"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyCWF1-fJvUSlYBfx9K9HbO9b8ggQcZydk4", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON(completionHandler: { response in
+            print(response)
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
